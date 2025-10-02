df5b4beba92f8f540e77722cd08fb3e340c5dfb8
mewrino <michellenguyen04272008@gmail.com>
2025-10-02 21:20:15 +0000

    x

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,32 @@
+public class Problem1
+{
+    public static void main(String[] args)
+    {
+        int num = 24;
+        num = num + 1;
+        System.out.println("number is now " + num);
 
+        num += 1;
+        System.out.printf("number is now %d%n", num);
+
+        num++;
+        System.out.println("number is now " + num);
+
+        num++;
+        System.out.println("number is now " + num);
+
+        num--;
+        System.out.println("number is now " + num);
+
+        num--;
+        System.out.println("number is now " + num); 
+
+        num--;
+        System.out.println("number is now " + num); 
+
+        num--;
+        System.out.println("number is now " + num); 
+
+
+    }
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,8 @@
+public class Problem2
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Three digit number: 678");
+        System.out.println("6");jav
 
+        I
